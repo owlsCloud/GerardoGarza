beb2f26f69e245a4a77591293bc912c0d74185e3
Gerardo Garza <owlscloud@gmail.com>
2023-02-19 18:14:54 -0600

    styled nav links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaBriefcase, FaEnvelope, FaHome, FaUser } from "react-icons/fa";
 function Navbar() {
+  const [currentlyActive, setCurrentlyActive] = useState("home");
   return (
-    <nav className="fixed right-0 h-screen flex ">
+    <nav className="fixed right-0 h-screen flex justify-center mr-10 ">
       <ul className="m-auto">
-        <li className="m-10">
-          <a href="#a">
-            <FaHome size={"1.3em"} color={"#ffffff"} />
+        <li
+          className={`w-[50px] h-[50px] ${
+            currentlyActive === "home" ? "bg-primary" : "bg-inActive"
+          } rounded-full m-auto mb-3 flex justify-center `}
+        >
+          <a href="#a" className="m-auto">
+            <FaHome size={"1.3em"} color={"#ffffff"} className="" />
           </a>
         </li>
-        <li className="m-10">
-          <a href="#a">
+        <li
+          className={`w-[50px] h-[50px] ${
+            currentlyActive === "about" ? "bg-primary" : "bg-inActive"
+          } rounded-full m-auto mb-3 flex justify-center `}
+        >
+          <a href="#a" className="m-auto">
             <FaUser size={"1.3em"} color={"#ffffff"} />
           </a>
         </li>
-        <li className="m-10">
-          <a href="#a">
+        <li
+          className={`w-[50px] h-[50px] ${
+            currentlyActive === "portfolio" ? "bg-primary" : "bg-inActive"
+          } rounded-full m-auto mb-3 flex justify-center `}
+        >
+          <a href="#a" className="m-auto">
             <FaBriefcase size={"1.3em"} color={"#ffffff"} />
           </a>
         </li>
-        <li className="m-10">
-          <a href="#a">
+        <li
+          className={`w-[50px] h-[50px] ${
+            currentlyActive === "contact" ? "bg-primary" : "bg-inActive"
+          } rounded-full m-auto mb-3 flex justify-center `}
+        >
+          <a href="#a" className="m-auto hover:cursor-pointer">
             <FaEnvelope size={"1.3em"} color={"#ffffff"} />
           </a>
         </li>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      primary: "#004000",
+      inActive: "#353535",
+    },
     extend: {},
   },
   plugins: [require("tailwind-scrollbar-hide")],
