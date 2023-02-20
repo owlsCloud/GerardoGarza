eb2f4cb4cc91f4da6d8d1743716bd248a483c8b4
Gerardo Garza <owlscloud@gmail.com>
2023-02-19 21:33:26 -0600

    added active check

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,17 +5,29 @@ function Navbar() {
   return (
     <nav className="fixed right-0 h-screen flex justify-center mr-10 ">
       <ul className="m-auto">
-        <a href="#a" className="m-auto">
+        <a
+          href="#a"
+          className="m-auto"
+          onClick={() => {
+            setCurrentlyActive("home");
+          }}
+        >
           <li
             className={`w-[50px] h-[50px] ${
               currentlyActive === "home" ? "bg-primary" : "bg-inActive"
             } rounded-full m-auto mb-3 flex justify-center `}
           >
             <FaHome size={"1.3em"} color={"#ffffff"} className="m-auto" />
-          </li>{" "}
+          </li>
         </a>
 
-        <a href="#a" className="m-auto">
+        <a
+          href="#a"
+          className="m-auto"
+          onClick={() => {
+            setCurrentlyActive("about");
+          }}
+        >
           <li
             className={`w-[50px] h-[50px] ${
               currentlyActive === "about" ? "bg-primary" : "bg-inActive"
@@ -25,7 +37,13 @@ function Navbar() {
           </li>
         </a>
 
-        <a href="#a" className="m-auto">
+        <a
+          href="#a"
+          className="m-auto"
+          onClick={() => {
+            setCurrentlyActive("portfolio");
+          }}
+        >
           <li
             className={`w-[50px] h-[50px] ${
               currentlyActive === "portfolio" ? "bg-primary" : "bg-inActive"
@@ -34,7 +52,13 @@ function Navbar() {
             <FaBriefcase size={"1.3em"} color={"#ffffff"} className="m-auto" />
           </li>
         </a>
-        <a href="#a" className=" hover:cursor-pointer">
+        <a
+          href="#a"
+          className=" "
+          onClick={() => {
+            setCurrentlyActive("contact");
+          }}
+        >
           <li
             className={`w-[50px] h-[50px] ${
               currentlyActive === "contact" ? "bg-primary" : "bg-inActive"
