0d502ff7227d4dbfcc8e138e5c0692f2060f20db
Gerardo Garza <owlscloud@gmail.com>
2023-02-19 18:18:20 -0600

    styled nav links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,42 +5,44 @@ function Navbar() {
   return (
     <nav className="fixed right-0 h-screen flex justify-center mr-10 ">
       <ul className="m-auto">
-        <li
-          className={`w-[50px] h-[50px] ${
-            currentlyActive === "home" ? "bg-primary" : "bg-inActive"
-          } rounded-full m-auto mb-3 flex justify-center `}
-        >
-          <a href="#a" className="m-auto">
-            <FaHome size={"1.3em"} color={"#ffffff"} className="" />
-          </a>
-        </li>
-        <li
-          className={`w-[50px] h-[50px] ${
-            currentlyActive === "about" ? "bg-primary" : "bg-inActive"
-          } rounded-full m-auto mb-3 flex justify-center `}
-        >
-          <a href="#a" className="m-auto">
-            <FaUser size={"1.3em"} color={"#ffffff"} />
-          </a>
-        </li>
-        <li
-          className={`w-[50px] h-[50px] ${
-            currentlyActive === "portfolio" ? "bg-primary" : "bg-inActive"
-          } rounded-full m-auto mb-3 flex justify-center `}
-        >
-          <a href="#a" className="m-auto">
-            <FaBriefcase size={"1.3em"} color={"#ffffff"} />
-          </a>
-        </li>
-        <li
-          className={`w-[50px] h-[50px] ${
-            currentlyActive === "contact" ? "bg-primary" : "bg-inActive"
-          } rounded-full m-auto mb-3 flex justify-center `}
-        >
-          <a href="#a" className="m-auto hover:cursor-pointer">
-            <FaEnvelope size={"1.3em"} color={"#ffffff"} />
-          </a>
-        </li>
+        <a href="#a" className="m-auto">
+          <li
+            className={`w-[50px] h-[50px] ${
+              currentlyActive === "home" ? "bg-primary" : "bg-inActive"
+            } rounded-full m-auto mb-3 flex justify-center `}
+          >
+            <FaHome size={"1.3em"} color={"#ffffff"} className="m-auto" />
+          </li>{" "}
+        </a>
+
+        <a href="#a" className="m-auto">
+          <li
+            className={`w-[50px] h-[50px] ${
+              currentlyActive === "about" ? "bg-primary" : "bg-inActive"
+            } rounded-full m-auto mb-3 flex justify-center `}
+          >
+            <FaUser size={"1.3em"} color={"#ffffff"} className="m-auto" />
+          </li>
+        </a>
+
+        <a href="#a" className="m-auto">
+          <li
+            className={`w-[50px] h-[50px] ${
+              currentlyActive === "portfolio" ? "bg-primary" : "bg-inActive"
+            } rounded-full m-auto mb-3 flex justify-center `}
+          >
+            <FaBriefcase size={"1.3em"} color={"#ffffff"} className="m-auto" />
+          </li>
+        </a>
+        <a href="#a" className=" hover:cursor-pointer">
+          <li
+            className={`w-[50px] h-[50px] ${
+              currentlyActive === "contact" ? "bg-primary" : "bg-inActive"
+            } rounded-full m-auto flex justify-center `}
+          >
+            <FaEnvelope size={"1.3em"} color={"#ffffff"} className="m-auto" />
+          </li>
+        </a>
       </ul>
     </nav>
   );
