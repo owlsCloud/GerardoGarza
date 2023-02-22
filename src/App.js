fd57f632a9b8b32f5652752d07d9d7e4974a7328
Gerardo Garza <owlscloud@gmail.com>
2023-02-21 19:07:39 -0600

    addec transition

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import { AnimatePresence, motion } from "framer-motion";
 import { Route, Routes } from "react-router-dom";
 import About from "./components/About";
 import MainPage from "./components/MainPage";
 import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <div className="scrollbar-hide">
       <Navbar />
+
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="about" element={<About />} />
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion } from "framer-motion";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
@@ -6,7 +7,30 @@ import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <AnimatePresence mode="wait">
+    <BrowserRouter>
+      {" "}
+      <motion.div
+        initial="initialState"
+        animate="animateState"
+        exit="exitState"
+        transition={{ duration: 0.75 }}
+        variants={{
+          initialState: {
+            clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
+            opacity: 0,
+          },
+          animateState: {
+            clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
+            opacity: 1,
+          },
+          exitState: {
+            clipPath: "polygon(50% 0, 50% 0, 50% 100%, 50% 100%)",
+          },
+        }}
+      >
+        <App />
+      </motion.div>
+    </BrowserRouter>
+  </AnimatePresence>
 );
