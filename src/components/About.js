362462fdd3aae68c2b289c00ec44363082e35281
Gerardo Garza <owlscloud@gmail.com>
2023-02-23 16:32:36 -0600

    added info

--- src/components/About.js
+++ src/components/About.js
@@ -2,14 +2,28 @@ import React from "react";
 
 function About() {
   return (
-    <div id="aboutMe" className="flex justify-center h-screen">
-      <h1 className="mt-10 text-[56px] text-white font-bold">
+    <section id="aboutMe" className="h-screen">
+      <h1 className=" p-10 text-[56px] text-white font-bold flex justify-center ">
         About <span className="text-primary ">Me</span>
       </h1>
-      <div></div>
-      <div></div>
-      <div></div>
-    </div>
+      <div className="flex justify-center w-1/2">
+        <div className="text-white  ">
+          <h5>Personal Info</h5>
+          <ul>
+            <li>Name: Gerardo Garza</li>
+            <li>Email: gjgarza31@gmail.com</li>
+          </ul>
+          <ul>
+            <li>Location: Edinburg, TX</li>
+            <li>Open to Contract/Fulltime/Parttime employment</li>
+          </ul>
+          <button className="rounded-full bg-textColor py-1 px-5 mt-2">
+            Resume
+          </button>
+        </div>
+        <div className=""></div>
+      </div>
+    </section>
   );
 }
 
